ea9bba6295a244f784b9fa71e156b263a8f2f584
lwoodson <lance@webmaneuvers.com>
2014-03-01 16:06:55 -0600

    Node parent/child relationships + path

--- lib/entitree/node.rb
+++ lib/entitree/node.rb
@@ -1,18 +1,38 @@
-require 'forwardable'
-
 module Entitree
   class Node
-    attr_reader :model, :ref_key
-    def initialize(model, ref_key=nil)
+    attr_reader :model, :ref_key, :reflection
+    attr_accessor :parent
+    def initialize(model, opts={})
       @model = model
-      @ref_key = ref_key
+      @ref_key = opts[:ref_key]
+      @parent = opts[:parent]
+      @reflection = opts[:reflection]
     end
 
-    extend Forwardable
-    def_delegator :model, :reflections, :reflections
+    def path
+      if root?
+        model.to_s
+      else
+        "#{parent.path}/#{ref_key}"
+      end
+    end
 
-    def reflection
-      reflections[ref_key]
+    def associated_nodes
+      @associated_nodes ||= model.reflections.map do |key, reflection|
+        Node.new(reflection.klass, ref_key: key, parent: self, reflection: reflection)
+      end
+    end
+
+    def nodes_above
+      associated_nodes.select{|node| node.belongs_to?}
+    end
+
+    def nodes_below
+      associated_nodes.select{|node| node.has_many?}
+    end
+
+    def nodes_aside
+      associated_nodes.select{|node| node.has_one? || node.has_and_belongs_to_many?}
     end
 
     def other_model
@@ -20,28 +40,28 @@ module Entitree
     end
 
     def root?
-      reflection.nil?
+      parent.nil?
     end
     alias_method :entity?, :root?
 
     def has_one?
-      reflection.macro == :has_one
+      reflection && reflection.macro == :has_one
     end
 
     def belongs_to?
-      reflection.macro == :belongs_to
+      reflection && reflection.macro == :belongs_to
     end
 
     def has_many?
-      reflection.macro == :has_many
+      reflection && reflection.macro == :has_many
     end
 
     def has_and_belongs_to_many?
-      reflection.macro == :has_and_belongs_to_many
+      reflection && reflection.macro == :has_and_belongs_to_many
     end
 
     def through?
-      reflection.options[:through].present?
+      reflection && reflection.options[:through].present?
     end
   end
 end
--- test/test_node.rb
+++ test/test_node.rb
@@ -1,13 +1,27 @@
 require_relative 'test_helper'
 
 describe Entitree::Node do
-  let(:root_node) {Entitree::Node.new(Post)}
-  let(:belongs_to_node) {Entitree::Node.new(Post, :author)}
-  let(:has_one_node) {Entitree::Node.new(Author, :author_detail)}
-  let(:has_one_through_node) {Entitree::Node.new(Author, :address)}
-  let(:has_many_node) {Entitree::Node.new(Author, :posts)}
-  let(:has_many_through_node) {Entitree::Node.new(Author, :comments)}
-  let(:has_and_belongs_to_many_node) {Entitree::Node.new(Post, :tags)}
+  let(:root_node) {Entitree::Node.new(Author)}
+  let(:child_node) {Entitree::Node.new(Post, ref_key: :posts, parent: root_node)}
+  let(:grandchild_node) {Entitree::Node.new(Comment, ref_key: :comments, parent: child_node)}
+  let(:belongs_to_node) {
+    Entitree::Node.new(Post, reflection: Post.reflections[:author])
+  }
+  let(:has_one_node) {
+    Entitree::Node.new(Author, reflection: Author.reflections[:author_detail])
+  }
+  let(:has_one_through_node) {
+    Entitree::Node.new(Author, reflection: Author.reflections[:address])
+  }
+  let(:has_many_node) {
+    Entitree::Node.new(Author, reflection: Author.reflections[:posts])
+  }
+  let(:has_many_through_node) {
+    Entitree::Node.new(Author, reflection: Author.reflections[:comments])
+  }
+  let(:has_and_belongs_to_many_node) {
+    Entitree::Node.new(Post, reflection: Post.reflections[:tags])
+  }
 
   describe "#reflection" do
     it "should be nil when a ref_key not present" do
@@ -19,13 +33,78 @@ describe Entitree::Node do
     end
   end
 
+  describe "#path" do
+    it "should be the model when it is a root/entity" do
+      root_node.path.must_equal "Author"
+    end
+
+    it "should be the root model joined with associations using a / separator if not a root" do
+      child_node.path.must_equal "Author/posts"
+    end
+
+    it "should work with arbitrarily nested associations" do
+      grandchild_node.path.must_equal "Author/posts/comments"
+    end
+  end
+
+  describe "#associated_nodes" do
+    subject {Entitree::Node.new(Author)}
+    let(:associated_node_paths) {subject.associated_nodes.map(&:path)}
+
+    it "should include has_one nodes" do
+      associated_node_paths.must_include "Author/author_detail"
+    end
+
+    it "should include has_one through nodes" do
+      associated_node_paths.must_include "Author/address"
+    end
+
+    it "should include has_many nodes" do
+      associated_node_paths.must_include "Author/posts"
+    end
+
+    it "should include has_many through nodes" do
+      associated_node_paths.must_include "Author/comments"
+    end
+
+    it "should include has_and_belongs_to_many nodes" do
+      Entitree::Node.new(Post).associated_nodes.map(&:path).must_include "Post/tags"
+    end
+  end
+
+  describe "#nodes_above" do
+    it "should only include nodes for belongs_to relationships" do
+      Entitree::Node.new(Post).nodes_above.map(&:path).must_equal ["Post/author"]
+    end
+  end
+
+  describe "#nodes_below" do
+    it "should include only nodes for the has_many relationships of the node's model" do
+      paths = Entitree::Node.new(Author).nodes_below.map(&:path)
+      paths.must_include "Author/posts"
+      paths.must_include "Author/comments"
+      paths.wont_include "Author/author_detail"
+      paths.wont_include "Author/address"
+    end
+  end
+
+  describe "#nodes_aside" do
+    it "should include only nodes for the has_one and has_and_belongs_to_many relationships" do
+      paths = Entitree::Node.new(Author).nodes_aside.map(&:path)
+      paths.must_include "Author/author_detail"
+      paths.must_include "Author/address"
+      paths.wont_include "Author/posts"
+      paths.wont_include "Author/comments"
+    end
+  end
+
   describe "#root?" do
     it "should be true when ref_key not present" do
       root_node.root?.must_equal true
     end
 
     it "should be false when ref_key present" do
-      Entitree::Node.new(Post, :comments).root?.must_equal false
+      child_node.root?.must_equal false
     end
   end
 
